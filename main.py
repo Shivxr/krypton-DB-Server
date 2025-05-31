0a5e2f73f2100dba412ec4bb2c3c0fed34b10de2
Shiv_Shankar <165561744+Shivxr@users.noreply.github.com>
2025-05-31 23:50:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from collections import deque as dq
-import threading
 
 app = Flask(__name__)
 
 k_id = 0
 l = -1
-mp = {}
+mp = {"":""}
 branches = []
 att = []
 
@@ -20,16 +19,35 @@ def right(ind, k_id):
         return ind * 2 + 2
     return -1
 
-def ins():
-    global k_id, l
-    for i in att:
-        v = input(f"enter value for attribute {i} : ")
-        branches.append(v)
+@app.route('/initialize', methods=['POST'])
+def initialize():
+    global k_id
+    global l
+    data = request.get_json()  # Expecting data in JSON format
+    for i in data['attributes']:
+        s = i
+        att.append(s)
+        mp[s] = len(mp)-1
+    return jsonify({"message": "Attributes initialized successfully"}), 200
+
+@app.route('/insert', methods=['POST'])
+def insert_data():
+    global k_id
+    global l
+    data = request.get_json()
+    new_branch = data['branch']
+    branches.extend(new_branch)
+
     if k_id == 0:
         l = 0
     k_id += 1
 
-def kfind(uid, branches, f, t):
+    return jsonify({"message": "Data inserted successfully"}), 200
+
+@app.route('/search/<int:uid>', methods=['GET'])
+def search(uid):
+    global branches
+    result = {}
     if l != -1:
         i, j = 0, k_id
         while i <= j:
@@ -39,104 +57,53 @@ def kfind(uid, branches, f, t):
             elif mid < uid:
                 i = mid + 1
             else:
-                if f == 1:
-                    print(branches[(mid * n):(mid * n) + n])
-                    break
-                else:
-                    print(branches[mid * n + mp[t]])
-                    break
+                return jsonify(branches[(mid * len(att)):(mid * len(att)) + len(att)]),200
 
-def bcheck(ind, branches, bname):
-    return branches[(ind * n) + mp[bname]]
+@app.route('/delete/<int:uid>', methods=['DELETE'])
+def delete(uid):
+    global branches
+    i = uid * len(att)
+    branches.pop(i)
+    return jsonify({"message": "Data deleted successfully"}), 200
 
-def kdel(uid):
-    i = uid * n
-    for q in range(n):
-        print(i)
-        branches.pop(i)
+@app.route('/scan', methods=['GET'])
+def kscan():
 
-def kscan(branches, wc, cond, bname, f):
-    if l != -1:
+    scan_type = int(request.args.get('scan_type', 1))
+    where_column = request.args.get('where_column', '')
+    condition = request.args.get('condition', '')
+    bname = request.args.get('bname', '')
+    f = scan_type
+    results = []
+    if l!= -1:
         d = dq([0])
         while d:
             z = d.popleft()
             lft, rgt = left(z, k_id), right(z, k_id)
-            if f == 0:
-                print(branches[(z * n):(z * n) + n])
-            elif f == 1:
-                if bcheck(z, branches, wc) == cond:
-                    print(branches[(z * n):(z * n) + n])
+
+            if f == 1:
+                # Entire scan: return the whole branch
+                results.append(branches[(z * len(att)):(z * len(att)) + len(att)])
+            elif f == 2:
+                # Scan branches with condition
+                if bcheck(z, where_column) == condition:
+                    results.append(branches[(z * len(att)):(z * len(att)) + len(att)])
             else:
-                if bcheck(z, branches, wc) == cond:
-                    print(bcheck(z, branches, bname))
+                # Scan fields with condition
+                if bcheck(z, where_column) == condition:
+                    results.append(bcheck(z, bname))
+
             if lft != -1:
                 d.append(lft)
             if rgt != -1:
                 d.append(rgt)
 
-def krypton():
-    global n
-    n = int(input("enter number of attributes : "))
-    for i in range(n):
-        s = input("enter attribute name : ")
-        att.append(s)
-        mp[s] = i
+    return jsonify({"results": results}), 200
 
-    while True:
-        print("1. Input data")
-        print("2. Search data")
-        print("3. Scan data")
-        print("4. Delete data")
-        print("5. Exit")
+ 
 
-        a = int(input())
+def bcheck(ind, bname):
+    return branches[(ind * len(att)) + mp[bname]]
 
-        if a == 1:
-            ins()
-        elif a == 2:
-            print("1. Entire branch")
-            print("2. Target field")
-            f = int(input())
-            gurt = int(input("enter id to search : "))
-            if f == 1:
-                kfind(gurt, branches, f, "")
-            else:
-                target = input("enter target attribute name : ")
-                kfind(gurt, branches, f, target)
-        elif a == 3:
-            print("1. scan entire")
-            print("2. scan branches with condition")
-            print("3. Scan fields with condition")
-
-            f = int(input())
-
-            if f == 1:
-                kscan(branches, "", "", "", 0)
-            elif f == 2:
-                bn = input("enter attribute name : ")
-                wc = input("enter attributer for where clause : ")
-                co = input("enter condition : ")
-                kscan(branches, wc, co, bn, 1)
-            else:
-                bn = input("enter attribute name : ")
-                wc = input("enter attributer for where clause : ")
-                co = input("enter condition : ")
-                kscan(branches, wc, co, bn, 2)
-        elif a == 4:
-            uid = int(input("enter uid to delete : "))
-            kdel(uid)
-            k_id -= 1
-        else:
-            break
-
-# Run krypton in a thread so it doesn't block Flask
-def run_krypton():
-    krypton()
-
-@app.route("/")
-def start_krypton():
-    threading.Thread(target=run_krypton).start()
-    return "Krypton started in terminal. Check your console."
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
